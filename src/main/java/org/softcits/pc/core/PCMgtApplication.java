448281c726c31941dc84b16abaa044bf95cb8c17
Jun <wangzhen_tju@126.com>
2018-04-22 15:19:54 +0800

    使SpringBoot支持war

---
+++ src/main/java/org/softcits/pc/core/PCMgtApplication.java
@@ -0,0 +1,13 @@
+package org.softcits.pc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PCMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PCMgtApplication.class, args);
+	}
+
+}
