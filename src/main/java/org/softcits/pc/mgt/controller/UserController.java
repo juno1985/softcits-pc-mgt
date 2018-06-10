979293b4eef01569e601c585f83e7fadf5b5b262
Jun <wangzhen_tju@126.com>
2018-06-10 10:43:13 +0800

    完成从auth得到token写入cookie

--- src/main/java/org/softcits/pc/mgt/controller/UserController.java
+++ src/main/java/org/softcits/pc/mgt/controller/UserController.java
@@ -1,8 +1,13 @@
 package org.softcits.pc.mgt.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.softcits.pc.mgt.common.CookieUtils;
 import org.softcits.pc.mgt.model.MbgUser;
 import org.softcits.pc.mgt.service.MgtUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,11 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.alibaba.druid.util.StringUtils;
+
 @Controller
 public class UserController {
 	
 	@Autowired
 	private MgtUserService mgtUserService;
+	
+	@Value("${COOKIE_AUTH_KEY}")
+	private String COOKIE_AUTH_KEY;
 	
 	@RequestMapping(path="/login", method=RequestMethod.GET)
 	public String goLoginView() {
@@ -22,11 +32,17 @@ public class UserController {
 	}
 	
 	@RequestMapping(path="/login", method=RequestMethod.POST)
-	public String login(@RequestParam String username, @RequestParam String passwd) {
+	public String login(@RequestParam String username, @RequestParam String passwd, HttpServletRequest request, HttpServletResponse response) {
 		
-		mgtUserService.login(username, passwd);
-		
-		return "redirect:/";
+		String token = mgtUserService.login(username, passwd);
+		//登录成功的情况
+		if(!StringUtils.isEmpty(token)) {
+			//将返回的token写入cookie
+			CookieUtils.setCookie(request, response, COOKIE_AUTH_KEY, token);
+			return "redirect:/";
+		}
+		//登录失败则返回登录页面
+		return "redirect:/login";
 	}
 
 	@RequestMapping(path="/user/update", method=RequestMethod.POST)
